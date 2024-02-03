6153b37abca3231729d946ed406013afee51e9d4
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-02-03 16:21:56 +0700

    feat: added navbar components

--- client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <div>Halo</div>
-      <button className="btn">Button</button>
-    </>
-  )
-}
---
+++ client/src/components/navbar/NavbarDropdownResponsive.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavbarDropdownResponsive = () => {
+  return (
+    <>
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </div>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-300 rounded-box w-52">
+          <li><a href="/" className="p-4 text-lg">Home</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default NavbarDropdownResponsive
